a95ffcdc93ef828faaab0fb71d0f1b479cd9e2dc
MohammadHajjaj <hajjaj.mohammad99@gmail.com>
2023-11-04 16:43:30 +0300

    implement create, update and delete pokemons endpoints

--- src/pokemon/v1/pokemon.controller.ts
+++ src/pokemon/v1/pokemon.controller.ts
@@ -1,5 +1,16 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import {
+  Controller,
+  Body,
+  Post,
+  ValidationPipe,
+  Get,
+  Delete,
+  Param,
+  ParseIntPipe,
+  Patch,
+} from '@nestjs/common';
 import { PokemonV1Service } from './pokemon.service';
+import { CreatePokemonDto, UpdatePokemonDto } from './dto';
 
 @Controller('v1/pokemons')
 export class PokemonV1Controller {
@@ -7,5 +18,25 @@ export class PokemonV1Controller {
   @Get(':pokemonId')
   getPokemonById(@Param('pokemonId', ParseIntPipe) pokemonId: number) {
     return this.pokemonService.getPokemonById(pokemonId);
+  }
+
+  @Post()
+  async createPokemon(
+    @Body(ValidationPipe) createPokemonDto: CreatePokemonDto,
+  ) {
+    return this.pokemonService.createPokemon(createPokemonDto);
+  }
+
+  @Patch(':pokemonId')
+  async updatePokemon(
+    @Param('pokemonId', ParseIntPipe) pokemonId: number,
+    @Body() updatePokemonDto: UpdatePokemonDto,
+  ) {
+    return await this.pokemonService.updatePokemon(pokemonId, updatePokemonDto);
+  }
+
+  @Delete(':pokemonId')
+  deletePokemonById(@Param('pokemonId', ParseIntPipe) pokemonId: number) {
+    return this.pokemonService.deletePokemonById(pokemonId);
   }
 }
--- src/pokemon/v1/pokemon.service.ts
+++ src/pokemon/v1/pokemon.service.ts
@@ -4,6 +4,7 @@ import {
   InternalServerErrorException,
 } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { CreatePokemonDto, UpdatePokemonDto } from './dto';
 
 @Injectable()
 export class PokemonV1Service {
@@ -21,8 +22,61 @@ export class PokemonV1Service {
 
       return pokemon;
     } catch (error) {
+      if (error instanceof NotFoundException) {
+        throw error;
+      }
       throw new InternalServerErrorException(
         `Error retrieving the Pokemon: ${error.message}`,
+      );
+    }
+  }
+
+  async createPokemon(createPokemonDto: CreatePokemonDto) {
+    try {
+      const newPokemon = await this.prisma.pokemon.create({
+        data: createPokemonDto,
+      });
+      return newPokemon;
+    } catch (error) {
+      throw new InternalServerErrorException(
+        `Error creating the Pokemon: ${error.message}`,
+      );
+    }
+  }
+
+  async updatePokemon(pokemonId: number, updatePokemonDto: UpdatePokemonDto) {
+    try {
+      const existingPokemon = await this.prisma.pokemon.findUnique({
+        where: { pokemonId },
+      });
+
+      if (!existingPokemon) {
+        throw new NotFoundException(`Pokemon with ID ${pokemonId} not found.`);
+      }
+
+      return await this.prisma.pokemon.update({
+        where: { pokemonId },
+        data: updatePokemonDto,
+      });
+    } catch (error) {
+      if (error instanceof NotFoundException) {
+        throw error;
+      }
+      throw new InternalServerErrorException(
+        `Error updating the Pokemon: ${error.message}`,
+      );
+    }
+  }
+
+  async deletePokemonById(pokemonId: number): Promise<void> {
+    try {
+      await this.prisma.pokemon.delete({
+        where: { pokemonId },
+      });
+    } catch (error) {
+      console.error('Error deleting the Pokemon:', error.message);
+      throw new InternalServerErrorException(
+        `An error occurred while deleting the Pokemon ${error.message}`,
       );
     }
   }
