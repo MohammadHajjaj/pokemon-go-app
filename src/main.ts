e8b95d0753cc66a724763b55191f63e02329ffbc
Mohammad Hajjaj <75132949+MohammadHajjaj@users.noreply.github.com>
2023-11-05 22:31:21 +0300

    Add Swagger API documenation (#7)

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule } from '@nestjs/swagger';
+import * as fs from 'fs';
+import * as YAML from 'yaml';
 import { AppModule } from './app.module';
 import { PrismaExceptionFilter } from './common/filters/prisma-exception.filter';
 
@@ -13,6 +16,10 @@ async function bootstrap() {
     }),
   );
   app.useGlobalFilters(new PrismaExceptionFilter());
+  const yamlDocument = YAML.parse(
+    fs.readFileSync('./documentation/api.yaml', 'utf8'),
+  );
+  SwaggerModule.setup('api', app, yamlDocument);
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT', 8080);
   await app.listen(port);
